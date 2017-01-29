c5d16f10f6568cb3942c1a164cd3a20b7f2f86f4
Gerrit Beine <gerrit.beine@gmx.de>
2017-01-29 18:54:38 +0100

    Update Emailchemy to 13.2.5

--- Casks/emailchemy.rb
+++ Casks/emailchemy.rb
@@ -1,6 +1,6 @@
 cask 'emailchemy' do
-  version '13.2.3'
-  sha256 '9155482447465e594cf70f926218e07f90c1cfbdd944e22fef6970d28675dda5'
+  version '13.2.5'
+  sha256 '42cf5f8a9e650d7b72a42ca85ffcb166ab94a61c579501430046da04bb728ca1'
 
   url "https://s3.amazonaws.com/wksdownload/Emailchemy-Mac.dmg"
   name 'Emailchemy'

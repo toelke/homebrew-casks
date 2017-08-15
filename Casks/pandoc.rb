3865b47033be1e62b4f0cffeb717edbe2f64de39
Gerrit Beine <gerrit.beine@gmx.de>
2017-08-15 20:11:24 +0200

    Update to 1.19.2.1

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,6 +1,6 @@
 cask 'pandoc' do
-  version '1.19.1'
-  sha256 '6b055e43d8b0d7f632150c3d82d02fc5ed6127f41e69471c79bde475e86e4f0b'
+  version '1.19.2.1'
+  sha256 '677ff56424de19d0f7f368aa9d9ece1b3fe03d7bd3e6bd0ad8f347debfcfafe5'
 
   url "https://github.com/jgm/pandoc/releases/download/#{version}/pandoc-#{version}-osx.pkg"
   name 'Pandoc'

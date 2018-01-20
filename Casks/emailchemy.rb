6a154e7d4cf568af2b40f0cd9e2002dad74496e0
Gerrit Beine <gerrit.beine@gmx.de>
2018-01-20 13:58:12 +0100

    Update Emailchemy to 14.0.2

--- Casks/emailchemy.rb
+++ Casks/emailchemy.rb
@@ -1,16 +1,12 @@
 cask 'emailchemy' do
-  version '13.2.10'
-  sha256 '3a4792b83fbec5d78fbc2d17c670cc8a81ea07915ccfc4c0d2b64d1260b3e728'
+  version '14.0.2'
+  sha256 '1cbe2b86cce12b5c5c98b7eac0f819b077b65c122a13ceefda8252b7837b4526'
 
   url "https://s3.amazonaws.com/wksdownload/Emailchemy-Mac.dmg"
   name 'Emailchemy'
   homepage 'http://weirdkid.com/emailchemy/'
 
-  installer script: 'Emailchemy Installer.app/Contents/MacOS/JavaApplicationStub',
-            args:   ['-q'],
-            sudo:   true
-
-  uninstall delete: '/Applications/Emailchemy.app'
+  app 'Emailchemy.app'
 
   caveats do
     depends_on_java

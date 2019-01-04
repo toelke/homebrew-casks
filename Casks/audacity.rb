97b1dafa81d5ab65c39e1ff92c5da8fb0e7d345d
Gerrit Beine <mail@gerritbeine.com>
2019-01-04 15:34:14 +0100

    Removed diplaylink drivers, available via homebrew/cask-drivers

---
+++ Casks/audacity.rb
@@ -0,0 +1,10 @@
+cask 'audacity' do
+  version '2.2.2'
+  sha256 '39a2783c78cca13e893321e0d87098739258cabbfc257119ee6cb21d0dc9037c'
+
+  url "https://www.audacityteam.org/audacity--2.2.2.dmg"
+  name 'Audacity'
+  homepage 'https://www.audacityteam.org/'
+
+  app 'Audacity.app'
+end

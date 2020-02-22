d42bd88c92a2d859ec38417a4253ec15e922e214
Gerrit Beine <mail@gerritbeine.com>
2020-02-22 17:03:24 +0100

    Added Fresh

---
+++ Casks/fresh.rb
@@ -0,0 +1,11 @@
+cask 'fresh' do
+  version '2.8.5'
+  sha256 '293cc98a16eb7e7bcad095ec71e2e8def8ad08fc9c7b084033a4beebd6bb519c'
+
+  url 'https://www.ironicsoftware.com/downloads/Fresh.zip'
+  appcast 'https://ironicsoftware.com/downloads/fresh.xml'
+  name 'Fresh'
+  homepage 'https://www.ironicsoftware.com/fresh/'
+
+  app 'Fresh.app'
+end

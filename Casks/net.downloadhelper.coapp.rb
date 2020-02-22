5823cd6b0e1b3b87ca0edd927c6d21b993ecb56d
Gerrit Beine <mail@gerritbeine.com>
2020-02-22 15:57:22 +0100

    Fixed brew style offenses...

--- Casks/net.downloadhelper.coapp.rb
+++ Casks/net.downloadhelper.coapp.rb
@@ -2,12 +2,13 @@ cask 'net.downloadhelper.coapp' do
   version '1.3.0'
   sha256 '92a7cc542fda84a2e2ca96c115cfaacad5586ea48751cd26579a823df2264797'
 
+  # github.com/mi-g/vdhcoapp was verified as official when first introduced to the cask
   url "https://github.com/mi-g/vdhcoapp/releases/download/v#{version}/net.downloadhelper.coapp-#{version}.pkg"
+  appcast 'https://github.com/mi-g/vdhcoapp/releases.atom'
   name 'Video DownloadHelper Companion App'
   homepage 'https://www.downloadhelper.net/install-coapp'
 
   pkg "net.downloadhelper.coapp-#{version}.pkg"
 
   uninstall pkgutil: 'net.downloadhelper.coapp'
-
 end

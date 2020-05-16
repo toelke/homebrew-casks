e1933c575b1b0c0b00218942cf6c2c44c9fb0659
Gerrit Beine <mail@gerritbeine.com>
2020-05-16 11:20:22 +0200

    Update pandoc to version 2.9.2.1

--- Casks/pandoc.rb
+++ Casks/pandoc.rb
@@ -1,6 +1,6 @@
 cask 'pandoc' do
-  version '2.9.2'
-  sha256 '23b15416a4a1c2c64b5dbc4f23c480b11feb34dcf38a65f6346dc8faa7feab0b'
+  version '2.9.2.1'
+  sha256 '6f2ef40dcc0edf6646bc4ed0622c5c33dd82c82c303a35bd9166d9f10561fc94'
 
   # github.com/jgm/pandoc was verified as official when first introduced to the cask
   url "https://github.com/jgm/pandoc/releases/download/#{version}/pandoc-#{version}-macOS.pkg"

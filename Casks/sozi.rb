d80ff5c27354e7149ab74e742539e283e24cf0a2
Philipp Tölke <pt@philipptoelke.de>
2021-03-31 18:07:07 +0200

    fix url

--- Casks/sozi.rb
+++ Casks/sozi.rb
@@ -3,7 +3,7 @@ cask 'sozi' do
   sha256 '6f017625d5a1f753218d1ae036790e7aa0cb5d26c89dfd271482725151059293'
 
   # github.com/senshu/Sozi was verified as official when first introduced to the cask
-  url "https://github.com/senshu/Sozi/releases/download/v#{version}/Sozi-21.02.21-1613918986-osx-x64.tgz"
+  url "https://github.com/sozi-projects/Sozi/releases/download/v#{version}/Sozi-21.02.21-1613918986-osx-x64.tar.xz"
   appcast 'https://github.com/senshu/Sozi/releases.atom'
   name 'Sozi'
   homepage 'https://sozi.baierouge.fr/'

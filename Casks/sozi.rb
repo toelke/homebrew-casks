637949ae95473c2a99f8ec90eeeb27652c6510d3
Philipp Tölke <pt@philipptoelke.de>
2021-03-31 18:00:30 +0200

    version bump sozi

--- Casks/sozi.rb
+++ Casks/sozi.rb
@@ -1,12 +1,13 @@
 cask 'sozi' do
-  version '19.04'
-  sha256 'e9bad8dd85e25a6b4679bc737e7d7f0e56f34f2abc5ffae937241b4c5a6ea166'
+  version '21.02'
+  longversion '21.02.21-1613918986'
+  sha256 '6f017625d5a1f753218d1ae036790e7aa0cb5d26c89dfd271482725151059293'
 
   # github.com/senshu/Sozi was verified as official when first introduced to the cask
-  url "https://github.com/senshu/Sozi/releases/download/v#{version}/Sozi-#{version}.22-1555944973-osx-x64.tgz"
+  url "https://github.com/senshu/Sozi/releases/download/v#{version}/Sozi-#{longversion}-osx-x64.tgz"
   appcast 'https://github.com/senshu/Sozi/releases.atom'
   name 'Sozi'
   homepage 'https://sozi.baierouge.fr/'
 
-  app "Sozi-#{version}.22-1555944973-osx-x64/Sozi.app"
+  app "Sozi-#{longversion}-osx-x64/Sozi.app"
 end
